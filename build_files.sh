114035d367247af7eddfc76e47fdfdb4c44b2657
bryson-20 <bryankimtai330@gmail.com>
2023-11-18 17:27:26 +0300

    small changes

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,2 @@
-# Create a virtual environment
-echo "Creating a virtual environment..."
-python -m pip install wheel setuptools pip --upgrade
-python3 -m pip install wheel setuptools pip --upgrade
-py -m pip install wheel setuptools pip --upgrade
-python3.9 -m pip install -r requirements.txt
-
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
-echo "BUILD END"
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
--- server/settings.py
+++ server/settings.py
@@ -141,11 +141,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
