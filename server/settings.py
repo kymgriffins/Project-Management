e9daf607183a50ee3a44534c91ce53421b9de61a
bryson-20 <bryankimtai330@gmail.com>
2023-11-20 11:26:23 +0300

    Monday's work

--- server/settings.py
+++ server/settings.py
@@ -20,6 +20,7 @@ import dj_database_url
 import os
 import environ
 
+
 # ENVIRONMENT
 env = environ.Env()
 environ.Env.read_env()
@@ -31,7 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rsa^2##xuiju7qjjvrluesf!f*14y!=i5z7f(^=+7=ls$gnl_4'
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # IS_HEROKU = "DYNO" in os.environ
